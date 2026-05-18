d1a13e0d0a279dd948ba8706909857c1075223b3
ASAPH <asaph1214@gmail.com>
2026-05-18 12:16:58 +0200

    lancement du ma premiere fenetre

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+#[macroquad::main("Mon jeu")]
+async fn main() {
+    loop {
+        clear_background(DARKBLUE);
+
+        draw_circle(
+            screen_width() / 2.0,
+            screen_height() / 2.0,
+            50.0, YELLOW
+        );
+
+        draw_text(
+            "bonjour macroquad",
+            20.0, 30.0, 30.0,
+            WHITE
+        );
+
+        next_frame().await;
+    }
 }
